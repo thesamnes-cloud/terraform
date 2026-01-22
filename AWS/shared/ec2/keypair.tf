828331e7f8b5b9d9e6d5e34605590dad24137e06
thesamnes-cloud <thesamnes@gmail.com>
2026-01-22 08:36:32 -0500

    Update keypair.tf

--- AWS/shared/ec2/keypair.tf
+++ AWS/shared/ec2/keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "app_server" {
   key_name   = "prod-mindbio-server"
-    public_key = file("~/.ssh/prod-mindbio-server.pub")
+  public_key = file("${path.root}/shared/keys/prod-mindbio-server.pub")
 }
